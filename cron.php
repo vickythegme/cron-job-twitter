5b2c0942827473f8bbc4822eb879df1c91292e7e
Vignesh <vickythegme@gmail.com>
2014-07-14 08:57:16 +0530

    Create cron.php
    added the code to tweet

---
+++ cron.php
@@ -0,0 +1,13 @@
+<?php
+//include the abraham's twitteroauth library
+require_once ('twitteroauth/twitteroauth.php');
+require_once ('config.php');
+//create an app and get the settings from dev.twitter.com
+$consumerKey = ""; //add the key from your app
+$consumerSecret = ""; //add the secret from your app
+$accessToken = ""; //add the access token from your app
+$accessSecret = ""; //add the access secret from your app
+$message = "Why don't you try this one?";
+$connection = new TwitterOAuth($consumerKey,$consumerSecret,$accessToken,$accessSecret);
+$connection -> post('statuses/update', array('status' => $message ));
+?>
